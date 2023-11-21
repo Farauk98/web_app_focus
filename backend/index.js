0ea52cd698b92832bd283b90a8c1272734362cc0
Rafał Kornat <kornat.rafal@gmail.com>
2023-11-21 18:25:40 +0000

    init frontend

--- backend/index.js
+++ backend/index.js
@@ -18,3 +18,34 @@ const number2 = req.params.number2;
  dialer.call(number1,number2);
  res.json({success: true});
 })
+
+const cors = require('cors');
+const bodyParser = require('body-parser');
+httpServer.use(bodyParser.json());
+httpServer.use(cors());
+httpServer.use((req, res, next) => {
+ res.header("Access-Control-Allow-Origin", "*");
+ res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
+ next();
+});
+httpServer.post('/call/', async (req, res) => {
+    const number1 = req.body.number;
+    const number2 = '517253860' // tutaj dejemy swój numer
+     console.log('Dzwonie', number1, number2)
+    const bridge = await dialer.call(number1, number2);
+    let interval = setInterval(async () => {
+     let status = await bridge.getStatus();
+     console.log(status)
+     if (
+     status === "ANSWERED" ||
+     status === "FAILED" ||
+     status === "BUSY" ||
+     status === "NO ANSWER"
+     ) {
+     console.log("stop");
+     clearInterval(interval);
+     }
+     }, 2000);
+     res.json({ success: true });
+    })
+    
