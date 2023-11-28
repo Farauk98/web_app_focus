df9a4e27b1d7ee0668139ff7dc853075a783fd59
Rafał Kornat <kornat.rafal@gmail.com>
2023-11-28 19:06:56 +0000

    update 6.3 warszatat#2

--- backend/index.js
+++ backend/index.js
@@ -1,3 +1,4 @@
+const { Server } = require('socket.io');
 const express = require('express');
 const httpServer = express();
 const dialer = require('dialer').Dialer;
@@ -7,11 +8,14 @@ const config = {
  login: 'focus23',
  password: '#b09pohnqfbi'
 };
+
 dialer.configure(config);
 // Serwer nasłuchuje na porcie 3000
-httpServer.listen(3000, function () {
+const serverInstance = httpServer.listen(3000, function () {
  console.log('Example app listening on port 3000!')
 })
+
+const io = new Server(serverInstance)
 httpServer.get('/call/:number1/:number2', (req, res) => {
 const number1 = req.params.number1;
 const number2 = req.params.number2;
@@ -28,24 +32,28 @@ httpServer.use((req, res, next) => {
  res.header("Access-Control-Allow-Headers", "Origin, Content-Type, Accept");
  next();
 });
-httpServer.post('/call/', async (req, res) => {
+httpServer.post('/call', async (req, res) => {
     const number1 = req.body.number;
     const number2 = '517253860' // tutaj dejemy swój numer
      console.log('Dzwonie', number1, number2)
-    const bridge = await dialer.call(number1, number2);
-    let interval = setInterval(async () => {
-     let status = await bridge.getStatus();
-     console.log(status)
-     if (
-     status === "ANSWERED" ||
-     status === "FAILED" ||
-     status === "BUSY" ||
-     status === "NO ANSWER"
-     ) {
-     console.log("stop");
-     clearInterval(interval);
-     }
-     }, 2000);
-     res.json({ success: true });
-    })
-    
+     bridge = await dialer.call(number1, number2);
+ let oldStatus = null
+let interval = setInterval(async () => {
+ let currentStatus = await bridge.getStatus();
+ if (currentStatus !== oldStatus) {
+ oldStatus = currentStatus
+ io.emit('status', currentStatus)
+ }
+ if (
+ currentStatus === "ANSWERED" ||
+ currentStatus === "FAILED" ||
+ currentStatus === "BUSY" ||
+ currentStatus === "NO ANSWER"
+ ) {
+ console.log('stop')
+ clearInterval(interval)
+ }
+}, 1000)
+res.json({ id: '123', status: bridge.STATUSES.NEW
+});
+})
